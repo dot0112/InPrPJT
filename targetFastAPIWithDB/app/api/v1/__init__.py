29e518e4c55a2067bf679c80ffbaca6a423a9a08
dot0112 <dot0112dot0112@gmail.com>
2025-06-10 16:02:33 +0900

    25/06/10 16:02

--- targetFastAPIWithDB/app/api/v1/__init__.py
+++ targetFastAPIWithDB/app/api/v1/__init__.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 from .endpoints import select, stats
 
 router = APIRouter()
-router.include_router(stats.get_stats)
-router.include_router(select.catch_all)
+router.include_router(stats.router)
+router.include_router(select.router)
